0d14525afffab39f034ba84e35dfddf523a069e4
Ku-Bri <106645400+Ku-Bri@users.noreply.github.com>
2025-10-31 11:57:38 -0500

    Changing panel system from individual to buttons to a scene panel operator

---
+++ Assets/Scripts/PanelOperator.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOperator : MonoBehaviour
+{
+    public GameObject[] panels;
+    public bool[] panelActive;
+
+    public void ShowPanel(GameObject panel)
+    {
+        //panel.SetActive(!isPanelActive);
+        //isPanelActive = !isPanelActive;
+    }
+}
